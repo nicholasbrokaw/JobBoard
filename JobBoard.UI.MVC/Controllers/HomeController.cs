571a5eb2eb947640e6d93afb7e37579706de3af1
nicholasbrokaw <brokawnd@gmail.com>
2019-12-02 15:16:56 -0600

    Added Identity and tweaked configuration.

--- JobBoard.UI.MVC/Controllers/HomeController.cs
+++ JobBoard.UI.MVC/Controllers/HomeController.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace JobBoard.UI.MVC.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        [HttpGet]
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
