b58bf102ff7f149091c63774dd8e34eda91a223a
nicholasbrokaw <brokawnd@gmail.com>
2019-12-06 10:53:44 -0600

    Implementing contact form.

--- JobBoard.UI.MVC/Controllers/HomeController.cs
+++ JobBoard.UI.MVC/Controllers/HomeController.cs
@@ -1,30 +1,78 @@
-﻿using System.Web.Mvc;
+﻿using JobBoard.UI.MVC.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace JobBoard.UI.MVC.Controllers
 {
-    public class HomeController : Controller
-    {
-        [HttpGet]
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
+		[HttpGet]
+		[Authorize]
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your app description page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        [HttpGet]
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		[HttpGet]
+		public ActionResult Help()
+		{
+			return View();
+		}
 
-            return View();
-        }
-    }
+		[HttpPost]
+		public ActionResult Help(ContactViewModel cvm)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(cvm);
+			}
+
+			string message = $"Message from: {cvm.FullName} at {cvm.Email}<br />Message: {cvm.Message}";
+
+			MailMessage mm = new MailMessage("admin@nicholasbrokaw.com", "brokawnd@gmail.com", cvm.Subject, message)
+			{
+				IsBodyHtml = true,
+				Priority = MailPriority.High
+			};
+
+			mm.ReplyToList.Add(cvm.Email);
+
+			SmtpClient client = new SmtpClient("mail.nicholasbrokaw.com")
+			{
+				Credentials = new NetworkCredential("d1908nbrokaw", "Twinkie829604")
+			};
+
+			try
+			{
+				client.Send(mm);
+			}catch(Exception ex)
+			{
+				ViewBag.ErrorMessage = ex.StackTrace;
+				return View(cvm);
+			}
+
+			return RedirectToAction("Contact");
+		}
+
+		[HttpPost]
+		public ActionResult BuildEmail(ContactViewModel cvm)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(cvm);
+			}
+
+			return Content("", "application/json");
+		}
+	}
 }
---
+++ JobBoard.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobBoard.UI.MVC.Models
+{
+	public class ContactViewModel
+	{
+		[Required(ErrorMessage = "*")]
+		public string FullName { get; set; }
+
+		[Required(ErrorMessage = "*")]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "*")]
+		[StringLength(100, ErrorMessage = "Value can be a maximum of 100 characters")]
+		public string Subject { get; set; }
+
+		[Required(ErrorMessage = "*")]
+		public string Message { get; set; }
+	}
+}
