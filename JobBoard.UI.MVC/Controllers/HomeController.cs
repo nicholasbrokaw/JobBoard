1e740c01ebe8d22c0be4042b1accc8f68241a57c
nicholasbrokaw <brokawnd@gmail.com>
2019-12-10 12:20:34 -0600

    Revert "Beginning to enhance UI"
    This reverts commit d4b5590202156e15206ae6c77869cbb58f0fa769.

--- JobBoard.UI.MVC/Controllers/HomeController.cs
+++ JobBoard.UI.MVC/Controllers/HomeController.cs
@@ -18,6 +18,15 @@ namespace JobBoard.UI.MVC.Controllers
 
 		[HttpGet]
 		[Authorize]
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your app description page.";
+
+			return View();
+		}
+
+		[HttpGet]
+		[Authorize]
 		public ActionResult Help()
 		{
 			return View();
